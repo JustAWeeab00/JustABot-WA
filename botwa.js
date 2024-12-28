ae31d2fd14f28d52b05865f43c207c4f6fc19850
Just A Weeaboo <152065444+JustAWeeab00@users.noreply.github.com>
2024-12-28 19:30:16 +0700

    Update botwa.js

--- botwa.js
+++ botwa.js
@@ -89,7 +89,9 @@ async function processImageSticker(mediaData, textToOverlay = "Sticker", msg) {
         .composite([{
             input: Buffer.from(`
                 <svg width="512" height="512" xmlns="http://www.w3.org/2000/svg">
-                    <text x="50%" y="50%" font-size="32" fill="white" text-anchor="middle" dominant-baseline="middle">${textToOverlay}</text>
+                    <text x="50%" y="50%" font-size="32" fill="white" fill-opacity="0.5" text-anchor="middle" dominant-baseline="middle">
+  ${textToOverlay}
+</text>
                 </svg>
             `),
             top: 0,
